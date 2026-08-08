1efa7edac20a08a4ec2bc6b40b362ce25a18cbe0
camilliverse <camilli.santos2015@gmail.com>
2026-08-07 21:56:29 -0300

    new line

--- 2026-4/POOII/aula01-convenio-service/src/main/java/br/edu/unifaj/cc/poo/aula01_convenio_service/controller/PacienteController.java
+++ 2026-4/POOII/aula01-convenio-service/src/main/java/br/edu/unifaj/cc/poo/aula01_convenio_service/controller/PacienteController.java
@@ -1,5 +1,27 @@
 package br.edu.unifaj.cc.poo.aula01_convenio_service.controller;
 
+import br.edu.unifaj.cc.poo.aula01_convenio_service.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class PacienteController {
-    
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping("/paciente/consulta")
+    public String participarConsulta(){
+        return pacienteService.participarConsulta();
+    }
+
+    @GetMapping("/paciente/exame")
+    public String fazerExame(){
+        return pacienteService.fazerExame();
+    }
+
+
 }
